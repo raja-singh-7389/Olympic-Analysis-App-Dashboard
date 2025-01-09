0474b0160938a4b7f3d8b3f1dcab45b1d650ac3a
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-09 07:54:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
+# Streamlit page configuration
 st.set_page_config(
-    page_title='Analyzer By raj',
+    page_title='Analyzer By Raj',
     page_icon='🧞‍♂️'
 )
+
+# Page title and header
 st.title(':rainbow[Data Analyzer For Everyone]')
-st.header(':gray[Explore Data with ease.]',divider='rainbow')
+st.header(':gray[Explore Data with ease.]', divider='rainbow')
 
-file = st.file_uploader('Drop csv or excel file',type['csv','xlsx'])
-if(file!=None):
-  if(file.name.endswith('csv')):
-      data = pd.read_csv(file)
-  else:
-      data = pd.read_excel(file)
+# File uploader
+file = st.file_uploader('Drop CSV or Excel file', type=['csv', 'xlsx'])
 
-st.dataframe(data)
-st.info('file uploaded succefully',icon='🧞‍♂️')
-
-
+if file is not None:
+    # Check file type and read data
+    if file.name.endswith('csv'):
+        data = pd.read_csv(file)
+    else:
+        data = pd.read_excel(file)
+    
+    # Display data and success message
+    st.dataframe(data)
+    st.info('File uploaded successfully!', icon='🧞‍♂️')
+else:
+    st.warning('Please upload a CSV or Excel file to get started.')
