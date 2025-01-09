639674e8a3881e076eb2203edfe2d2cd6298c44e
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-09 07:50:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,14 @@ st.set_page_config(
 st.title(':rainbow[Data Analyzer For Everyone]')
 st.header(':gray[Explore Data with ease.]',divider='rainbow')
 
-file = st.file_uploader('Drop csv or excel file')
+file = st.file_uploader('Drop csv or excel file',type['csv','xlsx'])
+if(file!=None):
+  if(file.name.endswith('csv')):
+      data = pd.read_csv(file)
+  else:
+      data = pd.read_excel(file)
+
+st.dataframe(data)
+st.info('file uploaded succefully',icon='🧞‍♂️')
+
+
